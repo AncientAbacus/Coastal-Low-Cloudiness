0321d3ac082798b7a959b2fcb45b3f5752e3cf21
Gino Angelici <ancientabacus@gmail.com>
2023-05-12 11:34:18 -0700

    Tapestry

--- Tapestry.py
+++ Tapestry.py
@@ -24,6 +24,10 @@ from Airport_CLC_Calculation import *
 #"""
 # Month by Latitude graph. Monthly CLC slopes. In red if significant. 1 plot
 
+# twenty aiport acronyms used for pacific rim summary + island airports KNSI and KNUC
+airport_acronyms = ["PADK", "PACD", "PADQ", "PAHO", "PYAK", "KSIT", \
+"PANT", "CYAZ", "KAST", "KOTH", "KACV", "KOAK", "KSFO", "KMRY", "KVBG", "KNTD", "KLAX", "KLGB", "KSAN", "KNZY", "KNSI", "KNUC"]
+
 #-------------------------------------------------------
 years = range(1950, 2023)
 
@@ -33,7 +37,21 @@ hours = [7, 10, 13, 16]
 
 elevation_def = 1000
 
-y_axis = "latitude"
+sort_by = "latitude"
+
+y_axis = airport_acronyms
 
 x_axis = months
 #-------------------------------------------------------
+
+airport_summary = pd.read_csv('Airport_Data_from_Sam/stationdata_RESforR.txt',delim_whitespace=True,names=["c1", "c2", "c3", "latitude", "c5", "elevation", "c7"])
+
+
+
+details = "Months_" + listToString(months) + "_Hours_" + listToString(hours) + "_Elevation_Definition_" + str(elevation_def) 
+
+# gets acronyms for all airports
+labels = pd.read_csv('Labels.csv', sep = "\t", names = ["acronyms", "locations"])
+labels = dict(labels.values[1::])
+
+plt.plot(months,airport_summary["airport_summary"])
